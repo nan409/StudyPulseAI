9dce05112a70dd917d5bdec7b0d26126ad01a8aa
nan409 <deadymannan@gmail.com>
2026-03-17 14:24:22 +0500

    Fix Verce depoyment and dependencies

--- api/index.py
+++ api/index.py
@@ -4,4 +4,6 @@ import os
 # Adjust path so the Vercel builder can load our backend logic from /backend
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'backend'))
 
-from app import app
+from app import create_app
+
+app = create_app()
